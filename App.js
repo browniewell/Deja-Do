34edffdd89969fccbcb64cfd0498ad123b18953c
rbrownewell <rbrownewell@medrhythms.com>
2022-03-31 10:49:06 -0400

    Added due date and start date

--- App.js
+++ App.js
@@ -5,22 +5,45 @@ import TodoListItem from "./components/todoListItem";
 
 
 class TodoItem{
-  constructor(title, daysRemaining, totalDuration){
+  // TODO: When we create a new item,
+  // -- If no start date is specified, use create time as start
+  // -- If start date and duration specified, infer due date as start date plus duration
+  // -- If no duration is specified, use time between start date and due date as the duration
+  // -- If if due date and duration specified, infer start date as the due date minus the duration
+  // TODO: Add duration to constructor, but check the above conditions to handle undefined/null params
+  constructor(title, dueDate, startDate){
     this.title = title;
-    this.daysRemaining = daysRemaining;
-    this.totalDuration = totalDuration;
+    this.dueDate = dueDate;
+    this.startDate = startDate;
   }
 
+  // FIXME: We can get into a situation where the Due Date comes before the Start date, so add protection for that
   getProgress(){
-    return (this.totalDuration - this.daysRemaining) / this.totalDuration;
+    function treatAsUTC(date) {
+      var result = new Date(date);
+      result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
+      return result;
+    }
+
+    function daysBetween(start, end){
+      var millisecondsPerDay = 24 * 60 * 60 * 1000;
+      var daysBetween = (treatAsUTC(end) - treatAsUTC(start)) / millisecondsPerDay;
+      return daysBetween;
+    }
+
+    var duration = daysBetween(this.startDate, this.dueDate);
+    var daysRemaining = daysBetween(Date.now(), this.dueDate);
+    var progress = (duration - daysRemaining) / duration;
+    
+    return progress;
   }
 }
 
 const App = () => {
   const [todos, setTodos] = useState([
-    new TodoItem("First Item", 10, 14),
-    new TodoItem("Second Item", 1, 3),
-    new TodoItem("Third Item", 1, 5)
+    new TodoItem("First Item", new Date(2022, 3, 15), new Date(2022, 2, 29)),
+    new TodoItem("Second Item", new Date(2022, 3, 1), new Date(2022, 2, 27)),
+    new TodoItem("Third Item", new Date(2022, 3, 2), new Date(2022, 2, 30))
   ]);
 
   return (
