dc5498ac75638e5c252d391eccf15c000c3edc36
rbrownewell <rbrownewell@medrhythms.com>
2022-03-31 15:55:01 -0400

    Infer due date from start date and duration

--- App.js
+++ App.js
@@ -4,46 +4,12 @@ import * as Progress from 'react-native-progress'
 import TodoListItem from "./components/todoListItem";
 
 
-class TodoItem{
-  // TODO: When we create a new item,
-  // -- If no start date is specified, use create time as start
-  // -- If start date and duration specified, infer due date as start date plus duration
-  // -- If no duration is specified, use time between start date and due date as the duration
-  // -- If if due date and duration specified, infer start date as the due date minus the duration
-  // TODO: Add duration to constructor, but check the above conditions to handle undefined/null params
-  constructor(title, dueDate, startDate){
-    this.title = title;
-    this.dueDate = dueDate;
-    this.startDate = startDate;
-  }
-
-  // FIXME: We can get into a situation where the Due Date comes before the Start date, so add protection for that
-  getProgress(){
-    function treatAsUTC(date) {
-      var result = new Date(date);
-      result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
-      return result;
-    }
-
-    function daysBetween(start, end){
-      var millisecondsPerDay = 24 * 60 * 60 * 1000;
-      var daysBetween = (treatAsUTC(end) - treatAsUTC(start)) / millisecondsPerDay;
-      return daysBetween;
-    }
-
-    var duration = daysBetween(this.startDate, this.dueDate);
-    var daysRemaining = daysBetween(Date.now(), this.dueDate);
-    var progress = (duration - daysRemaining) / duration;
-    
-    return progress;
-  }
-}
-
 const App = () => {
   const [todos, setTodos] = useState([
-    new TodoItem("First Item", new Date(2022, 3, 15), new Date(2022, 2, 29)),
-    new TodoItem("Second Item", new Date(2022, 3, 1), new Date(2022, 2, 27)),
-    new TodoItem("Third Item", new Date(2022, 3, 2), new Date(2022, 2, 30))
+    new TodoItem("First Item", new Date(2022, 2, 15), 30),
+    new TodoItem("Second Item", new Date(2022, 2, 25), 10),
+    new TodoItem("Third Item", new Date(2022, 2, 30), 5),
+    new TodoItem("Future Item", new Date(2022, 3, 3), 8),
   ]);
 
   return (
@@ -66,3 +32,42 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+
+class TodoItem{
+  constructor(title, startDate, duration){
+    this.title = title;
+    this.duration = duration;
+    this.startDate = startDate;
+    this.dueDate = null;
+  }
+
+  getProgress(){
+    function treatAsUTC(date) {
+      var result = new Date(date);
+      result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
+      return result;
+    }
+
+    function daysBetween(start, end){
+      var millisecondsPerDay = 24 * 60 * 60 * 1000;
+      var daysBetween = (treatAsUTC(end) - treatAsUTC(start)) / millisecondsPerDay;
+      return daysBetween;
+    }
+
+    this.dueDate = this.startDate.addDays(this.duration);
+
+    // FIXME: If the start date is in the future, use Start Date - Duration as the initial start date so we can calculate what the "progress" should show
+
+    var daysRemaining = daysBetween(Date.now(), this.dueDate);
+    var progress = (this.duration - daysRemaining) / this.duration;
+    
+    return progress;
+  }
+}
+
+Date.prototype.addDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
