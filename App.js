9d6672fc46e5074ee7395440b046c8f72fff235c
rbrownewell <rbrownewell@medrhythms.com>
2022-04-02 10:24:48 -0400

    Formatting and fixed days remaining calc

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-import React, { useState } from "react";
-import { FlatList, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
-import * as Progress from 'react-native-progress'
-import TodoListItem from "./components/todoListItem";
-
+import React, {useState} from 'react';
+import {
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import * as Progress from 'react-native-progress';
+import TodoListItem from './components/todoListItem';
 
 const App = () => {
   const [todos, setTodos] = useState([
-    new TodoItem("First Item", new Date(2022, 2, 15), 30),
-    new TodoItem("Second Item", new Date(2022, 2, 25), 10),
-    new TodoItem("Third Item", new Date(2022, 2, 30), 5),
-    new TodoItem("Future Item", new Date(2022, 3, 5), 5),
+    new TodoItem('First Item', new Date(2022, 3, 15), 30),
+    new TodoItem('Second Item', new Date(2022, 3, 5), 10),
+    new TodoItem('Third Item', new Date(2022, 3, 9), 5),
+    new TodoItem('Fourth Item', new Date(2022, 3, 2), 4),
   ]);
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={ todos }
-        renderItem={({ item }) => (
-          <TodoListItem item={ item } />
-        )}
+        data={todos}
+        renderItem={({item}) => <TodoListItem item={item} />}
       />
     </SafeAreaView>
   );
@@ -33,38 +38,40 @@ const styles = StyleSheet.create({
 
 export default App;
 
-
-class TodoItem{
-  constructor(title, dueDate, duration){
+class TodoItem {
+  constructor(title, dueDate, duration) {
     this.title = title;
     this.duration = duration;
     this.dueDate = dueDate;
     this.daysRemaining = null;
   }
 
-  getProgress(){
+  getProgress() {
     function treatAsUTC(date) {
       var result = new Date(date);
       result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
       return result;
     }
 
-    function daysBetween(start, end){
+    function daysBetween(start, end) {
       var millisecondsPerDay = 24 * 60 * 60 * 1000;
-      var daysBetween = (treatAsUTC(end) - treatAsUTC(start)) / millisecondsPerDay;
+      var daysBetween =
+        (treatAsUTC(end) - treatAsUTC(start)) / millisecondsPerDay;
       return daysBetween;
     }
 
     // FIXME: If the due date is farther out than the current date plus the duration, the progress bar will be empty until that is no longer the case. This is only applicable on the first occurrence.
     this.daysRemaining = daysBetween(Date.now(), this.dueDate);
     var progress = (this.duration - this.daysRemaining) / this.duration;
-    
+
+    this.daysRemaining = Math.ceil(this.daysRemaining);
+
     return progress;
   }
 }
 
-Date.prototype.addDays = function(days) {
+Date.prototype.addDays = function (days) {
   var date = new Date(this.valueOf());
   date.setDate(date.getDate() + days);
   return date;
-}
+};
